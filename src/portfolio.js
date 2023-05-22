2195c6b49a6f1afadc26afc7c67c532b60608480
youssef_ammar <youssef.ammar19@yahoo.com>
2023-05-22 03:19:39 +0100

    image

--- src/portfolio.js
+++ src/portfolio.js
@@ -377,7 +377,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "vme.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
